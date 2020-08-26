8df177f326b50f70e2d4e3dd7b3cb94cd776f803
surjkee <stasiklviv@gmail.com>
2020-08-26 14:24:07 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello world App
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          board: Array(25).fill(null),
+        }
+      }
+    render() {
+    const Box = this.state.board.map(box => <div className="box">{box}</div>)
+        return (
+            <div className="container">
+                <h1>GO Game</h1>
+                <div className="board">
+
+                {Box}
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
